9d7b500d2976ecdde709adcfbec4d1ef69ec2163
marcosmarcon <mvgmarcon@gmail.com>
2020-09-02 22:10:55 -0300

    testando a 1 classe de dominio categoria

--- src/main/java/com/example/demo/resources/CategoriaResources.java
+++ src/main/java/com/example/demo/resources/CategoriaResources.java
@@ -1,16 +1,29 @@
 package com.example.demo.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.categoria;
 
 @RestController
 @RequestMapping(value = "categoria") 
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET) 
-	public String listar() {
-		return "rest funfando";
+	public List<categoria> listar() {
+		
+		categoria cat1 = new categoria(1, "informatica");
+		categoria cat2 = new categoria(1, "escritorio");
+		
+		List<categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
